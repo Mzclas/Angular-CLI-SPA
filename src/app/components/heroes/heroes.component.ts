508995df3a3c333f18e4793f2b6eb46357b8bc24
mzclas <Mzclas@gmail.com>
2019-12-17 17:15:18 +0100

    terminado primer ejercicio

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HeroesService, Heroe } from "src/app/services/heroes. service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-heroes",
@@ -7,9 +8,12 @@ import { HeroesService, Heroe } from "src/app/services/heroes. service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Heroe[] = [];
-  constructor(private _heroesService: HeroesService) {}
+  constructor(private _heroesService: HeroesService, private router: Router) {}
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
+  }
+  seeHeroe(idx: number) {
+    this.router.navigate(["/heroe", idx]);
   }
 }
